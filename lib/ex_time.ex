50170b73515f432118e3d14d474a0ec0f5f44ece
Yuri Oliveira <soaresyuri@gmail.com>
2022-10-14 16:21:50 -0300

    Initial commit

---
+++ lib/ex_time.ex
@@ -0,0 +1,18 @@
+defmodule ExTime do
+  @moduledoc """
+  Documentation for `ExTime`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTime.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_time_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTimeTest do
+  use ExUnit.Case
+  doctest ExTime
+
+  test "greets the world" do
+    assert ExTime.hello() == :world
+  end
+end
